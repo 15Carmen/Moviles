286c62c9f77271f31deaa395ee8148dd83eeba6f
15Carmen <carmen.martin@iesnervion.es>
2023-10-04 21:18:07 +0200

    notificacion hecha (gracias isak)

---
+++ Calculadora/app/src/main/java/com/example/calculadora/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.calculadora
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import android.util.Log
+import android.widget.Toast
+
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+    }
+
+    override fun onResume() {
+        super.onResume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+    }
+
+    override fun onStop() {
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+}
+
+
