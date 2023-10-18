1032eeaa86f1868c5442ea59cc10a52466b636b0
15Carmen <carmen.martin@iesnervion.es>
2023-10-18 09:50:08 +0200

    putas fotos

--- Galeria/app/src/main/java/com/example/galeria/FotosAdapter.kt
+++ Galeria/app/src/main/java/com/example/galeria/FotosAdapter.kt
@@ -1,42 +1,46 @@
 package com.example.galeria
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.example.galeria.databinding.ActivityGaleriaBinding
 import com.example.galeria.databinding.ItemFotosBinding
 
-class FotosAdapter (private val fotos: List<Fotos>, private val fotoPulsadaListener: FotoPulsadaListener): RecyclerView.Adapter<FotosAdapter.ViewHolder>(){
+class FotosAdapter : RecyclerView.Adapter<FotosAdapter.ViewHolder>(){
 
-    class ViewHolder(val binding: ItemFotosBinding): RecyclerView.ViewHolder(binding.root){
-        fun bind(fotos: Fotos) {
-            Glide
-                .with(binding.root.context)
-                .load(fotos.imagenMiniUrl)
-                .into(binding.fotito);
-        }
+    var itemList: MutableList<Fotos> = ArrayList()
+    var context : Context? = null
 
+    class ViewHolder(val binding: ItemFotosBinding): RecyclerView.ViewHolder(binding.root)
+
+    fun AdaptadorFotos(Imagen:MutableList<Fotos>, context: Context){
+        this.itemList = Imagen
+        this.context = context
+    }
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = itemList?.get(position)
+
+        Glide.with(context!!.applicationContext)
+            .load(item?.imagenMiniUrl)
+            .centerCrop()
+            .diskCacheStrategy(DiskCacheStrategy.ALL)
+            .into(holder.binding.fotito)
 
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val binding= ItemFotosBinding.inflate(LayoutInflater.from(parent.context),parent,false )
-
-        return ViewHolder(binding)
+        return ViewHolder(
+            ItemFotosBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        )
     }
 
     override fun getItemCount(): Int {
-
-        return fotos.size
-    }
-
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        holder.bind(fotos[position])
-
-        holder.itemView.setOnClickListener{
-            fotoPulsadaListener.fotoPulsada(fotos[position])
-        }
+        return itemList!!.size
     }
 
 }
