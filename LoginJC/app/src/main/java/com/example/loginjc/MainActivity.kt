3150b4e9ba28db0b94b5a7732aa18179e0fbd51e
15Carmen <carmen.martin@iesnervion.es>
2023-10-25 12:49:03 +0200

    empieza a complicarse la cosa

---
+++ LoginJC/app/src/main/java/com/example/loginjc/MainActivity.kt
@@ -0,0 +1,113 @@
+package com.example.loginjc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.TextFieldValue
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.loginjc.ui.theme.LoginJCTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            LoginJCTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Login()
+                }
+            }
+        }
+    }
+}
+
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun Login(modifier: Modifier = Modifier) {
+
+    var username by remember { mutableStateOf(TextFieldValue("")) }
+    var password by remember { mutableStateOf(TextFieldValue("")) }
+
+
+    Column (
+        modifier = Modifier
+            .padding(10.dp)
+            .fillMaxSize(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+           ){
+
+        Image(painter = painterResource(id = R.drawable.this_is_fine),
+            contentDescription = "this is fine",
+            Modifier.height(200.dp)
+        )
+
+        TextField(
+            value = username,
+            onValueChange = {
+                username = it
+            },
+            placeholder = { Text(text = "Nombre de usurio") },
+            modifier = Modifier.padding(10.dp)
+        )
+        TextField(
+            value = password,
+            onValueChange = { password = it },
+            placeholder = { Text(text = "Contraseña")},
+            visualTransformation = PasswordVisualTransformation(),
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Password)
+
+        )
+
+        Button(onClick = { 
+            /*TODO*/ 
+        },
+            colors = ButtonDefaults.buttonColors(Color(255, 165, 0, 255)),
+            modifier = Modifier.padding(10.dp)
+
+        ) {
+            Text(text = "Log in")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    LoginJCTheme {
+        Login()
+    }
+}
